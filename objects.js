6e265f738f93e51f1c727a2fad4eedc03713c960
Orin Fletcher <orin.fletcher@gmail.com>
2017-09-25 22:21:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
 var playlist = {artistName:'songTitle'} //create object
 
 function updatePlaylist(playlist, artistName, songTitle){
+  //object playList will be update key -> artistName with value -> songTitle
   playList[artistName] = songTitle
+  
 }
